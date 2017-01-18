e82a3744eb3ad0c777b1dbfdccbb7768a654454b
ReenExe <ipourri@gmail.com>
2017-01-18 19:37:01 +0200

    key start from zero

--- simple.go
+++ simple.go
@@ -1,7 +1,15 @@
 package algorithm
 
+import "fmt"
+
 func findIndexesForSum(list []int, target int) []int {
 	result := []int{}
 	result = append(result, target)
+
+	for index, value := range list {
+		fmt.Println("index:", index)
+		fmt.Println("value:", value)
+	}
+
 	return result
 }
