7cf821070edafe23c461efee2c74a1859e2f4ddc
ReenExe <ipourri@gmail.com>
2017-01-18 19:48:01 +0200

    pass tests (#3)

--- simple.go
+++ simple.go
@@ -3,6 +3,10 @@ package algorithm
 func findIndexesForSum(list []int, target int) []int {
 	for index, value := range list {
 		for innerIndex, innerValue := range list {
+			if index == innerIndex {
+				continue
+			}
+
 			if value + innerValue == target {
 				return []int{index, innerIndex};
 			}
