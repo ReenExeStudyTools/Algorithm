01dad6e37946c9c863ddce64fbaac55227620757
ReenExe <ipourri@gmail.com>
2017-01-18 19:44:06 +0200

    debug twoSum (#3)

--- simple_test.go
+++ simple_test.go
@@ -3,10 +3,16 @@ package algorithm
 import (
 	"reflect"
 	"testing"
+	"fmt"
 )
 
 func TestPlayer(t *testing.T) {
 	if !reflect.DeepEqual(findIndexesForSum([]int{1, 2}, 3), []int{0, 1}) {
+		t.Fatal("fail")
+	}
+
+	fmt.Print(findIndexesForSum([]int{3, 2, 4}, 6))
+	if !reflect.DeepEqual(findIndexesForSum([]int{3, 2, 4}, 6), []int{1, 2}) {
 		t.Fatal("fail")
 	}
 }
