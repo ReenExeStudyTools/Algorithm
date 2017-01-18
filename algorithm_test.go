2948c16f1ad0770b11973a3d8b1504b147e0d0ea
ReenExe <ipourri@gmail.com>
2017-01-18 20:04:28 +0200

    test before code (#3)

--- algorithm_test.go
+++ algorithm_test.go
@@ -13,4 +13,10 @@ func TestPlayer(t *testing.T) {
 	if !reflect.DeepEqual(findIndexesForSum([]int{3, 2, 4}, 6), []int{1, 2}) {
 		t.Fatal("fail")
 	}
+
+	var listNode ListNode = addTwoListNode(&ListNode{7, nil}, &ListNode{8, nil})
+
+	if (listNode.Val != 15) {
+		t.Fatal("fail")
+	}
 }
