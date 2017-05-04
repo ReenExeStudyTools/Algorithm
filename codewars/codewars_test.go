21c42194c837a687f5b5fb731fb964aacbd6e615
ReenExe <ipourri@gmail.com>
2017-05-04 23:34:33 +0300

    test before code #7

--- codewars/codewars_test.go
+++ codewars/codewars_test.go
@@ -26,4 +26,8 @@ func TestTwoOldestAges(t *testing.T)  {
 	if !reflect.DeepEqual(TwoOldestAges([]int{6,5,83,5,3,18}), [2]int{18,83}) {
 		t.Fatal("fail")
 	}
+
+	if !reflect.DeepEqual(TwoOldestAges([]int{1,5,87,45,8,8}), [2]int{45,87}) {
+		t.Fatal("fail")
+	}
 }
